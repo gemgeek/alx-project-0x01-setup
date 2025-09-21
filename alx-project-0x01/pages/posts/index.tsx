729bb76201981dc65a97d33b646a1a81797758ab
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-09-21 10:32:22 +0100

    feat: Initial project setup

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PostsPage: React.FC = () => {
+  return <h1>Posts</h1>;
+};
+
+export default PostsPage;
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const UsersPage: React.FC = () => {
+  return <h1>Users</h1>;
+};
+
+export default UsersPage;
