27e1eab0669b6ac8492038b0475f97a4aa8867ff
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-09-21 11:05:11 +0100

    feat: Implement PostCard and fetch posts with getStaticProps

---
+++ alx-project-0x01/interfaces/index.ts
@@ -0,0 +1,15 @@
+export interface PillProps {
+  title: string;
+}
+
+export interface ButtonProps {
+  styles: string;
+  title: string;
+}
+
+export interface PostProps {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,15 +1,51 @@
+import PostCard from "@/components/common/PostCard";
+import Header from "@/components/layout/Header";
+import { PostProps } from "@/interfaces";
 import React from 'react';
-import Header from '@/components/layout/Header';
 
-const PostsPage: React.FC = () => {
+
+interface PostsPageProps {
+  posts: PostProps[];
+}
+
+const PostsPage: React.FC<PostsPageProps> = ({ posts }) => {
   return (
-    <div>
+    <div className="bg-gray-50 min-h-screen">
       <Header />
       <main className="container mx-auto p-4">
-        <h1 className="text-2xl font-bold">Posts</h1>
+        <div className="flex justify-between items-center mb-6">
+          <h1 className="text-3xl font-bold">All Posts</h1>
+          <button className="bg-blue-600 px-4 py-2 rounded-md text-white font-semibold hover:bg-blue-700 transition">Add Post</button>
+        </div>
+        {/* We map over the 'posts' array and render a PostCard for each item */}
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          {posts?.map((post) => (
+            <PostCard
+              key={post.id} 
+              id={post.id}
+              title={post.title}
+              body={post.body}
+              userId={post.userId}
+            />
+          ))}
+        </div>
       </main>
     </div>
-  );
-};
+  )
+}
+
+
+export async function getStaticProps() {
+  // Fetch data from the external API
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const posts: PostProps[] = await response.json();
+
+  // The data is returned as props to the page component
+  return {
+    props: {
+      posts, // will be passed to the PostsPage component as a prop
+    },
+  };
+}
 
 export default PostsPage;
