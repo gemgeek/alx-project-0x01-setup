74bfb344780a232ef01f5c89c8684dc8902ae6f9
Matilda Esenam Gbeve <esenam16@gmail.com>
2025-09-21 10:50:11 +0100

    feat: Implement Header and page navigation

--- alx-project-0x01/pages/posts/index.tsx
+++ alx-project-0x01/pages/posts/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Header from '@/components/layout/Header';
 
 const PostsPage: React.FC = () => {
-  return <h1>Posts</h1>;
+  return (
+    <div>
+      <Header />
+      <main className="container mx-auto p-4">
+        <h1 className="text-2xl font-bold">Posts</h1>
+      </main>
+    </div>
+  );
 };
 
 export default PostsPage;
